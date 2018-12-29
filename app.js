d270eb4ece7d2d476a4e3d6eb64157a4d07dd13e
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-12-29 13:20:44 +0100

    heal method

--- app.js
+++ app.js
@@ -10,9 +10,17 @@ new Vue({
             let randomNumberOne = Math.floor(Math.random() * 10) + n;
             let randomNumberTwo = Math.floor(Math.random() * 10) + n;
             this.hitPoints.push('Monster hits player for ' + randomNumberOne);
-            this.hitPoints.push('Player hits player for ' + randomNumberTwo);
+            this.hitPoints.push('Player hits monster for ' + randomNumberTwo);
             this.healthbarWidthOne -= randomNumberOne;
             this.healthbarWidthTwo -= randomNumberTwo;
+        },
+        heal() {
+            let randomNumberOne = Math.floor(Math.random() * 10) + 11;
+            let randomNumberTwo = Math.floor(Math.random() * 10) + 11;
+            this.hitPoints.push('Monster hits player for ' + randomNumberOne);
+            this.hitPoints.push('Player heals himself for ' + randomNumberTwo);
+            this.healthbarWidthOne -= randomNumberOne;
+            this.healthbarWidthOne += randomNumberTwo;
         }
     }
 });
