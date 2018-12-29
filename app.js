b1610025ecaa6e37884d1003d44dcf6273d14a3e
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-12-29 13:15:20 +0100

    one attack method

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ new Vue({
         hitPoints: []
     },
     methods: {
-        attack() {
-            let randomNumberOne = Math.floor(Math.random() * 10) + 1;
-            let randomNumberTwo = Math.floor(Math.random() * 10) + 1;
+        attack(n) {
+            let randomNumberOne = Math.floor(Math.random() * 10) + n;
+            let randomNumberTwo = Math.floor(Math.random() * 10) + n;
             this.hitPoints.push('Monster hits player for ' + randomNumberOne);
             this.hitPoints.push('Player hits player for ' + randomNumberTwo);
             this.healthbarWidthOne -= randomNumberOne;
             this.healthbarWidthTwo -= randomNumberTwo;
-            console.log(this.hitPoints)
         }
     }
 });
