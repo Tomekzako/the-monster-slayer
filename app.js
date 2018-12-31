580263a6b7be316d2274e70335ba0c0044750478
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-12-31 13:12:13 +0100

    startGame method

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 new Vue({
     el: "#app",
     data: {
-        healthbarWidthOne: 375,
-        healthbarWidthTwo: 375,
-        hitPoints: []
+        playerHealth: 100,
+        monsterHealth: 100,
+        hitPoints: [],
+        gameIsRunning: false
     },
     methods: {
+        startGame() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.hitPoints = []
+        },
         attack(n) {
             let randomNumberOne = Math.floor(Math.random() * 10) + n;
             let randomNumberTwo = Math.floor(Math.random() * 10) + n;
             this.hitPoints.push('Monster hits player for ' + randomNumberOne);
             this.hitPoints.push('Player hits monster for ' + randomNumberTwo);
-            this.healthbarWidthOne -= randomNumberOne;
-            this.healthbarWidthTwo -= randomNumberTwo;
+            this.playerHealth -= randomNumberOne;
+            this.monsterHealth -= randomNumberTwo;
         },
         heal() {
             let randomNumberOne = Math.floor(Math.random() * 10) + 11;
             let randomNumberTwo = Math.floor(Math.random() * 10) + 11;
             this.hitPoints.push('Monster hits player for ' + randomNumberOne);
             this.hitPoints.push('Player heals himself for ' + randomNumberTwo);
-            this.healthbarWidthOne -= randomNumberOne;
-            this.healthbarWidthOne += randomNumberTwo;
+            this.playerHealth -= randomNumberOne;
+            this.playerHealth += randomNumberTwo;
         }
+        
     }
 });
